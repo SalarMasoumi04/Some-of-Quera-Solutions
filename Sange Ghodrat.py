b361cf485f6c69b521380d3ac963be196760d907
S. Salar . Masoumi <113671169+SalarMasoumi04@users.noreply.github.com>
2024-02-06 16:10:16 +0330

    شش سنگ

---
+++ Sange Ghodrat.py
@@ -0,0 +1,16 @@
+X= input()
+
+if X=='time':
+    print('green')
+elif X=='space':
+    print('blue')
+elif X=='mind':
+    print('yellow')
+elif X=='reality':
+    print('red')
+elif X=='power':
+    print('purple')
+elif X=='soul':
+    print('orange')
+else:
+    print('none')    
